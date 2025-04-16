069cfcee25e446dceef05127b32873bb4440fe11
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 16:41:32 +0200

    TP1 : boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,7 +4,7 @@
 int main() {
     int compteur = 5;
     for (int i=0; i<compteur; i++) {
-        for (int j = 0; j<i; j++) {
+        for (int j = 0; j<=i; j++) {
             printf("*");
         }
         printf("\n");
