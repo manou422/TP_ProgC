4b40802f543e652b93b1814c92a5fb9f5ea0bd2f
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 14:56:48 +0200

    TP1 : cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -3,8 +3,13 @@
 #include <math.h>
 
 int main() {
-    float rayon;
-    printf("Quel est votre rayon ? ");
-    scanf("%.2f", &rayon);
+    float rayon = 3;
+    float aire = 0;
+    float périmètre = 0;
+    aire = M_PI * rayon * rayon;
+    périmètre = 2 * M_PI * rayon;
+    printf("%.2f m\n", aire);
     printf("%.2f m\n", rayon);
+    printf("%.2f m\n", périmètre);
+
 }
