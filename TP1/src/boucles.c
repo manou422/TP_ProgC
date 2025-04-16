ffa4e9ce6f6c50b58cc2676b942f47bf86c31e83
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 16:45:49 +0200

    TP1 : boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -5,7 +5,7 @@ int main() {
     int compteur = 5;
     for (int i=0; i<compteur; i++) {
         for (int j = 0; j<=i; j++) {
-            if((i==2 || i==3) && (j==1 || j==2)) {
+            if((i==2 || i==3) && (j==1 || j==2) && j!=i) {
                 printf("#");
             } else {
                 printf("*");
