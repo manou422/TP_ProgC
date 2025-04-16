945d8eb4914c8323188224c89d2d434c4ce02943
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 14:47:01 +0200

    TP1 : cercle.c

---
+++ TP1/src/cercle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <Math.h>
+
+int main() {
+    float rayon;
+    printf("Quel est votre rayon ? ");
+    scanf("%.2f", &rayon);
+    printf("%.2f m\n", rayon);
+}
