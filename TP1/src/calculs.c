6ac2848467b95ea8a15fc883096025e10259101e
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 17:16:59 +0200

    TP1 : calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
+int main() {
+    int num1 = 10;
+	int num2 = 5;
+	string op = '+'
+
+	switch (op) {
+		case + : return num1 + num2;
+		case - : return num1 - num2;
+		case * : return num1 * num2;
+		case / : return num1 / num2;
+		case % : return num1 % num2;
+		case & : return num1 & num2;
+		case | : return num1 | num2;
+		case ~ : return num1 ~ num2;
+	}
+
+}
