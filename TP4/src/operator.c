987ddbb0a3c117444aab0f07bb0de5541174865b
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 15:33:31 +0200

    TP1 : operateur.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -6,19 +6,19 @@ int a = 16;
 int b = 3;
 int res = 0
 res = a+b;
-printf("L'addition = %d", res);
+printf("L'addition = %d\n", res);
 
 res = a-b;
-printf("La soustraction = %d", res);
+printf("La soustraction = %d\n", res);
 
 res = a*b;
-printf("La multiplication = %d", res);
+printf("La multiplication = %d\n", res);
 
 res = a/b;
-printf("La division = %d", res);
+printf("La division = %d\n", res);
 
 res = a%b;
-printf("L'addition = %d", res);
+printf("L'addition = %d\n", res);
 
 bool egal = (a==b);
 printf("a est égal à b ? %s\n", egal ? "true" : "false");
