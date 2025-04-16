93736f5993754448c79b99f70236c0c2c768e932
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 15:11:56 +0200

    TP1 : sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Taille de char : %zu octet(s)\n", sizeof(char));
-    return 0;
+    printf("Taille de char           : %zu octet(s)\n", sizeof(char));
+    printf("Taille de short          : %zu octet(s)\n", sizeof(short));
+    printf("Taille de int            : %zu octet(s)\n", sizeof(int));
+    printf("Taille de long int       : %zu octet(s)\n", sizeof(long int));
+    printf("Taille de long long int  : %zu octet(s)\n", sizeof(long long int));
+    printf("Taille de float          : %zu octet(s)\n", sizeof(float));
+    printf("Taille de double         : %zu octet(s)\n", sizeof(double));
+    printf("Taille de long double    : %zu octet(s)\n", sizeof(long double));
 }
