82c0270de051b68317313bac5c3beb9110de01f7
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 15:28:40 +0200

    TP1 : operateur.c

---
+++ TP4/src/operator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+int a = 16;
+int b = 3;
+int res = 0
+res = a+b;
+printf("L'addition = %d", res);
+
+res = a-b;
+printf("La soustraction = %d", res);
+
+res = a*b;
+printf("La multiplication = %d", res);
+
+res = a/b;
+printf("La division = %d", res);
+
+res = a%b;
+printf("L'addition = %d", res);
+
+bool egal = (a==b);
+printf("a est égal à b ? %s\n", egal ? "true" : "false");
+
+bool superieur = (a>b)
+printf("a est égal à b ? %s\n", superieur ? "true" : "false");
+
+}
