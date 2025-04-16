a7ed7900cd6d4b5ecdeb4b357ad1c593ac5aa13e
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-16 15:37:36 +0200

    TP1 : operateur.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 main() {
-int a = 16;
-int b = 3;
-int res = 0
-res = a+b;
-printf("L'addition = %d\n", res);
+    int a = 16;
+    int b = 3;
+    int res = 0
+    res = a+b;
+    printf("L'addition = %d\n", res);
 
-res = a-b;
-printf("La soustraction = %d\n", res);
+    res = a-b;
+    printf("La soustraction = %d\n", res);
 
-res = a*b;
-printf("La multiplication = %d\n", res);
+    res = a*b;
+    printf("La multiplication = %d\n", res);
 
-res = a/b;
-printf("La division = %d\n", res);
+    res = a/b;
+    printf("La division = %d\n", res);
 
-res = a%b;
-printf("L'addition = %d\n", res);
+    res = a%b;
+    printf("L'addition = %d\n", res);
 
-bool egal = (a==b);
-printf("a est égal à b ? %s\n", egal ? "true" : "false");
+    bool egal = (a==b);
+    printf("a est égal à b ? %s\n", egal ? "true" : "false");
 
-bool superieur = (a>b)
-printf("a est égal à b ? %s\n", superieur ? "true" : "false");
+    bool superieur = (a>b)
+    printf("a est égal à b ? %s\n", superieur ? "true" : "false");
 
-return 0;
+    return 0;
 
 }
