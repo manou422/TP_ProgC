f8275ca89319832a2f9116c781dc7600045f99e9
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-30 15:46:20 +0200

    bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int d = 213852961890986541341787654310;
+    
+    int bit_4 = (d >> 3) & 1;
+    int bit_20 = (d >> 19) & 1;
+    
+    if (bit_4 == 1 && bit_20 == 1) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    return 0;
+}
