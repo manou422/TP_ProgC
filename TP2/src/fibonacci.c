1b2cb1e39a211283599c19c7dafed830ce38766e
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-30 17:02:05 +0200

    fibonnacci.c

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    init = 0;
+    therme1 = 1;
+    therme2 = init + therme1
+	thermeGeneral = 0;
+	for(int n = 0; i<n i++) {
+		thermegeneral += i; 
+		print(thermegeneral);
+    }
+}
