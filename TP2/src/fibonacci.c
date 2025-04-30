e4a7539fba7ee997172f1576197dd53d908181da
Mortiz32 <115144938+Mortiz32@users.noreply.github.com>
2025-04-30 17:05:07 +0200

    fibonacci.c

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -2,11 +2,12 @@
 #include <string.h>
 
 int main() {
+	int i = 7;
     int init = 0;
     int therme1 = 1;
-    int therme2 = init + therme1
+    int therme2 = init + therme1;
 	int thermeGeneral = 0;
-	for(int n = 0; i<n; i++) {
+	for(int i = 0; i<n; i++) {
 		thermegeneral += i; 
 		printf("%d\n", thermegeneral);
     }
